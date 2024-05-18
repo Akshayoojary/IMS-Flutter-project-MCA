8f4f92effb50395175e269c8ad66e3f6706c0382
Akshay Poojary <akshaypoojary167@gmail.com>
2024-05-18 15:15:12 +0530

    Flutter-first-commit-login-page

---
+++ ims/android/app/src/main/kotlin/com/example/ims/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ims
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ ims/lib/pages/auth_page.dart
@@ -0,0 +1,26 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:ims/pages/login_page.dart';
+import 'home_page.dart';
+
+class  AuthPage extends StatelessWidget{
+  const AuthPage({super.key});
+  @override 
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot){
+          //user logged in
+          if(snapshot.hasData){
+            return HomePage();
+          }
+          //user not loged in
+          else{
+            return LoginPage();
+          }
+        },
+      ),
+    );
+  }
+}
